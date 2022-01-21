3ace6be421163833ed4267b5b0e335cafee307a3
Pwmski <69732707+Pwmski@users.noreply.github.com>
2022-01-21 10:55:36 +0000

    Update 1c_fastqc.sh

--- 1c_fastqc.sh
+++ 1c_fastqc.sh
@@ -2,14 +2,16 @@
 
 #input filename at the end of the command
 
+#method1 - for loop on all files with fastq.gz suffix:
 for file in *fastq.gz; do
 
-fastqc -f fastq -t 7 -o /home/pwmski/oxbioinf/cutrun2_h3k14ac/fastq/FASTQC_reports $file
+fastqc -f fastq -t 7 -o /fastq/FASTQC_reports $file
 
 done
 
+#method2 - while loop with retaining sample names:
 ls *gz | cut -d _ -f 1-2 | sort | uniq | while read id; do
 
-fastqc -f fastq -t 7 -o /home/pwmski/oxbioinf/cutrun2_h3k14ac/fastq/FASTQC_reports/ ${id}
+fastqc -f fastq -t 7 -o /fastq/FASTQC_reports/ ${id}
 
 done
