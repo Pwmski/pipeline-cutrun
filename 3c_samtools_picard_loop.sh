97ec766d2155c41b55d197da9ac3e57616f8c26d
Pwmski <69732707+Pwmski@users.noreply.github.com>
2022-01-21 10:57:53 +0000

    Update 3c_samtools_picard_loop.sh

--- 3c_samtools_picard_loop.sh
+++ 3c_samtools_picard_loop.sh
@@ -4,47 +4,15 @@ ls *.sam | cut -d . -f 1 | sort | uniq \
     | while read id; do \
 
        samtools view -@7 -S -b ${id}.sam > ${id}.bam
-	   samtools sort -@7 ${id}.bam -o /home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted/${id}_sorted.bam
+	   samtools sort -@7 ${id}.bam -o /bam_sam_IP/bam_sorted/${id}_sorted.bam
 	   
 	   picard MarkDuplicates \
       REMOVE_DUPLICATES=true \
-      INPUT=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted/${id}_sorted.bam \
-      OUTPUT=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.bam \
-      METRICS_FILE=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.log \
+      INPUT=/bam_sam_IP/bam_sorted/${id}_sorted.bam \
+      OUTPUT=/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.bam \
+      METRICS_FILE=/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.log \
 	  
-	  samtools index /home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.bam
+	  samtools index /bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.bam
 
 
       done
-
-
-ls *.sam | cut -d . -f 1 | sort | uniq \
-    | while read id; do \
-
-       samtools view -@7 -S -b ${id}.sam > ${id}.bam
-	   samtools sort -@7 ${id}.bam -o /home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted/${id}_sorted.bam
-	   samtools index /home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted/${id}_sorted.bam
-
-
-      done
-
-ls *.bam | cut -d . -f 1 | sort | uniq \
-    | while read id; do \
-	   
-	  picard MarkDuplicates \
-      REMOVE_DUPLICATES=true \
-      INPUT=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted/${id}_sorted.bam \
-      OUTPUT=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.bam \
-      METRICS_FILE=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.log \
-	  
-	  samtools index /home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/${id}_sorted_rmvdups.bam
-
-      done
-	  
-	  
-	  picard MarkDuplicates \
-      REMOVE_DUPLICATES=true \
-      INPUT=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted/s28_sorted.bam \
-      OUTPUT=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/s28_sorted_rmvdups.bam \
-      METRICS_FILE=/home/pwmski/oxbioinf/cutrun2_h4k16ac/bam_sam_IP/bam_sorted_rmvdup/s28_sorted_rmvdups.log \
-	  
