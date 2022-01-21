0dc0637a20b1f1486a7e685746d301265d0193e4
Pwmski <69732707+Pwmski@users.noreply.github.com>
2022-01-21 11:00:04 +0000

    Update 6b_deeptools_multibigwigsummary.sh

--- 6b_deeptools_multibigwigsummary.sh
+++ 6b_deeptools_multibigwigsummary.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 	
-multiBigwigSummary BED-file --BED hela_taddomains_slop.bed --bwfiles /home/pwmski/oxbioinf/cutrun2_h3k14ac/deeptools_bamcoverage/bigwig_rmvdups/*.bw --numberOfProcessors max --smartLabels --verbose --outFileName /home/pwmski/oxbioinf/cutrun2_h3k14ac/deeptools_multibigwigsumm/h3k14ac_multibw_tads.npz --outRawCounts /home/pwmski/oxbioinf/cutrun2_h3k14ac/deeptools_multibigwigsumm/h3k14ac_multibw_tads.tab
+multiBigwigSummary BED-file --BED hela_taddomains_slop.bed --bwfiles /deeptools_bamcoverage/bigwig_rmvdups/*.bw --numberOfProcessors max --smartLabels --verbose --outFileName /deeptools_multibigwigsumm/h3k14ac_multibw_tads.npz --outRawCounts /deeptools_multibigwigsumm/h3k14ac_multibw_tads.tab
