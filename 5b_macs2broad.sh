6aaa0cb8a2ffab78631c3ab6e7b197766486a177
Pwmski <69732707+Pwmski@users.noreply.github.com>
2022-01-21 10:59:33 +0000

    Update 5b_macs2broad.sh

--- 5b_macs2broad.sh
+++ 5b_macs2broad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-outPath="/home/pwmski/oxbioinf/cutrun2_h3k14ac/macs2/peak_rmvdups_broad/" \
+outPath="/macs2/peak_rmvdups_broad/" \
 
 ls *rmvdups.bam | cut -d _ -f 1 | sort | uniq \
     | while read id; do \
@@ -8,14 +8,3 @@ ls *rmvdups.bam | cut -d _ -f 1 | sort | uniq \
       macs2 callpeak -t ${id}_sorted_rmvdups.bam -f BAMPE -g 2913022398 -q 0.1 --broad --broad-cutoff 0.1 -B --SPMR -n macs2rmvdups_${id} --outdir $outPath 2>${outPath}/macs2rmvdups_${id}.log
 
      done
-
-
-#outPath="/home/pwmski/oxbioinf/cutrun2_h4k16ac/macs2/peak_keepdups_broad/"
-#
-#ls *sorted.bam | cut -d _ -f 1 | sort | uniq \
-#    | while read id; do \
-#
-#     macs2 callpeak -t ${id}_sorted.bam -f BAMPE -g 2913022398 -q 0.1 --keep-dup all --broad --broad-cutoff 0.1 -B --SPMR -n #macs2keepdups_${id} --outdir $outPath 2>${outPath}/macs2keepdups_${id}.log
-#
-#    done
-#
