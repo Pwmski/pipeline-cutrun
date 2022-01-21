761d35c04817732d391b5fd77c6802cc85e12a3c
Pwmski <69732707+Pwmski@users.noreply.github.com>
2022-01-21 10:58:49 +0000

    Update 4a_deeptools_bamcoverage_loop.sh

--- 4a_deeptools_bamcoverage_loop.sh
+++ 4a_deeptools_bamcoverage_loop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#rmvdup
+#remove duplicate bam sorted file as input
 ls *rmvdups.bam | cut -d _ -f 1 | sort | uniq \
     | while read id; do \
 
-      bamCoverage --bam ${id}_sorted_rmvdups.bam -o /home/pwmski/oxbioinf/cutrun2_h3k14ac/deeptools_bamcoverage/bigwig_rmvdups/${id}_rmvdups_h3k14ac.bw \
+      bamCoverage --bam ${id}_sorted_rmvdups.bam -o /deeptools_bamcoverage/bigwig_rmvdups/${id}_rmvdups.bw \
     --scaleFactor 1 \
     --binSize 25 \
    --normalizeUsing CPM \
@@ -14,29 +14,3 @@ ls *rmvdups.bam | cut -d _ -f 1 | sort | uniq \
     --numberOfProcessors=7 \
 
       done
-
-#keep dup 
-ls *sorted.bam | cut -d _ -f 1 | sort | uniq \
-    | while read id; do \
-
-      bamCoverage --bam ${id}_sorted.bam -o /home/pwmski/oxbioinf/cutrun2_h3k14ac/deeptools_bamcoverage/bigwig_keepdups/${id}_keepdups_h3k14ac.bw \
-    --scaleFactor 1 \
-    --binSize 25 \
-    --normalizeUsing CPM \
-    --effectiveGenomeSize 2913022398 \
-    --ignoreForNormalization chrX \
-    --extendReads \
-    --numberOfProcessors=7 \
-
-      done
-
-      bamCoverage --bam s28_sorted_rmvdups.bam -o /home/pwmski/oxbioinf/cutrun2_h3k14ac/deeptools_bamcoverage/bigwig_rmvdups/s28_rmvdups_h3k14ac.bw \
-    --scaleFactor 1 \
-    --binSize 25 \
-    --normalizeUsing CPM \
-    --effectiveGenomeSize 2913022398 \
-    --ignoreForNormalization chrX \
-    --extendReads \
-    --numberOfProcessors=7 \
-
-	  
